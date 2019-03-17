98c90721357a9b949e3d7a7ec082b3adff2b9d34
Jake David <jakedgmu13@gmail.com>
2019-03-17 00:10:19 -0400

    wip hw 3.2

---
+++ assets/javascript/game.js
@@ -0,0 +1,112 @@
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wordBank = [abase	abet	abeyance	abjure	abstemious	abut	abysmal	accede	accentuate	accolade	accord	acme acquiesce	acrimony	actuate	adage	adjure	admonish	adulation	adulterate	adventitious	adverse advert	advocate	affable	affect	afflict	aggrieve	aide	alacrity	alcove	allay	allege	amalgamate ambrosia	amenable	amicable	animus	anodyne	anthropomorphic	aphorism	apocryphal	apologist apostate	apotheosis	apparent	appeal	appease	append	appoint	apposite	approach	appurtenance apropos	apt	aquiline	arbitrary	arbitrate	archetype	aria	armada	arrogate	ascendancy	ascertain askance	asperity	aspersion	assail	assay	assent	assert	assiduous	astringent	asunder	atavism attitude	attribute	audacious	augury	august	auspices	austere	automate	avarice	aversion	avocation axiom	badinage	baleful	banal	bandy	bane	banter	bate	bauble	beatific	beguile	beleaguer	belie bemuse	bemused	benighted	bent	bequeath	beseech	besiege	bestow	betoken	betrothed	bilious blanch	bland	blatant	blighted	bluster	boast	bolt	bouillon	brawn	brooch	brook	bucolic	bullion bulwark	burgeon	burnish	buxom	cabal	cadaverous	cajole	callous	cameo	campaign	candid	canon cant	canto	capitulate	caprice	capricious	captious	cardinal	careen	carrion	categorical	caucus cavalier	cede	chaff	chagrin	chamber	chary	chastise	checkered	chide	choleric	circumspect circumstances	circumvent	cite	clad	clandestine	clarion	cleave	coast	coax	codicil	cogent	cognate cohere	cohort	coiffure	collation	commiserate	commission	commit	compel	comport	comprehensive conceit	concept	concern	concomitant	conduct	confer	confluence	congeal	conjugal	conjure connotation	conscientious	consecrate	conservatory	consider	conspicuous	constant	constituency constitute	constrained	construe	contempt	contend	contract	contrive	contrived	contusion convalesce	convention	convince	corpulent	countermand	court	cower	coy	cozen	crabbed	craven credence	credible	credo	crevice	crew	crone	cryptic	cupidity	dais	dapper	dearth	debacle	debase decadence	declivity	decorum	decree	deduce	deference	defray	deign	delicacy	delve	demonstrable demoralize	denouement	dense	depose	deprave	depredation	derelict	derived	descry	despair despoil	device	devolve	diadem	dictum	didactic	diffidence	diffident	dilettante	dingy	dint disaffected	disapprobation	disconcert	disconsolate	discreet	discretionary	discriminating	discursive disembark	disgorge	dispassionate	dispatch	disport	disposition	disquisition	dissemble	dissimulate distend	distinction	diversified	divulge	docile	dogged	doggerel	dormer	dotage	dote	dregs	dulcet dutiful	dwell	dyspeptic	earnest	ebb	edict	edifice	effervescence	effete	effluvium	effulgence egregious	elaborate	ellipsis	elude	elysian	emblazon	emboss	embroil	emend	emetic	emolument enamor	enclave	encroachment	encumber	endemic	endow	endue	enduring	enervate	engage enigma	enjoin	enrapture	ensconce	ensue	entertain	entomology	entreat	equable	erode	errant erstwhile	eschew	espouse	establish	esteem	estrange	eugenic	euphony	evanescent	evident	exalt exasperate	excise	execrable	execrate	exegesis	exert	exertion	exigency	exorbitant	expatiate	expiate explicate	exploit	exploitation	expostulation	extenuate	extol	extort	extravagant	facile	faction factious	factitious	fad	fallow	fancy	fanfare	fare	fathom	fatuous	fawning	felicitous	fervent	fiasco filch	financial	firebrand	fissure	fitful	flag	flail	flair	flay	flotilla	flourish	foil	foist	foolhardy	ford forebear	foreboding	formal	forsake	fortitude	foster	fracas	fractious	fraught	fray	fresco	frieze frontier	fructify	frustrate	fulminate	functionary	furnish	gadfly	gainsay	gale	garble	garish	gaunt gawk	generate	genesis	gentility	gentry	gestate	gibe	gird	gist	glib	glower	gouge	granary grandiloquent	grant	grapple	gratis	gravity	gripe	grisly	grove	grovel	grudging	gruel	gusto	gusty hackneyed	haggle	halcyon	hale	halting	harrowing	harry	headlong	heed	herculean	hermitage	hew heyday	hireling	hoary	homespun	homily	hoodwink	horde	hubbub	humble	humiliate	hummock hurtle	husbandry	iconoclast	ideology	idiosyncrasy	ignominious	ignominy	illustrious	imbibe	immure immutable	impart	impel	impertinent	imperturbable	impetus	impiety	implacable	impolitic importunate	imposed	imposing	imposition	imposture	improvident	incandescent	incidental	incipient incisive	inclement	inclined	inclusive	incongruity	inconsequential	incorrigible	inculcate	indemnify indication	indite	infallible	infest	inflammatory	ingrained	ingratiate	inimical	inimitable	iniquitous innate	inscrutable	insensate	insinuate	insist	inspire	instance	institute	insurgent	intend	interdict interminable	interpolate	intimate	intrepid	intrigue	inured	invective	invidious	invocation	irksome irresolute	issue	jet	jibe	jocund	junket	justify	keen	kindle	knack	knight	knoll	labor	laggard lancet	languid	larder	largesse	latitude	league	level	levity	liberal	libertine	libretto	lineament livelihood	loath	lode	lofty	loquacious	lout	luminary	luxuriant	machination	madrigal	magisterial majority	makeshift	malaise	malcontent	malevolent	malice	malleable	manifest	manifestation mannered	manumit	maraud	masticate	maul	maxim	meander	mendicant	mercurial	merit	mettle microcosm	mien	mincing	minute	misanthrope	miserly	mite	mode	modest	modicum	monetary moot	morbid	mordant	moribund	mortify	mote	mountebank	multitude	mundane	munificent	muse muster	natty	nettle	nexus	nicety	nihilist	nominal	nonentity	nonplus	nostrum	notion notwithstanding	novel	nuance	obdurate	objective	obloquy	obtain	obtrude	obtuse	obviate	odium ominous	omniscient	onset	oppress	oratorio	ordain	ornate	ostensible	outskirts	overbearing overweening	pacifist	pacify	palatable	palatial	pall	palliate	pallid	palpitate	parable	paraphernalia parity	parochial	parry	parsimonious	partial	partiality	partisan	passage	passe	pastoral	pathological patrician	patronize	pedagogue	pedantic	pellucid	penchant	pendulous	peon	peremptory	perfidious perish	permeate	peroration	perpetrate	perpetual	persist	perspicuous	pert	pertinacious	pervade petulant	philology	philosophical	phlegmatic	pilfer	pillage	pinion	pious	pique	pithy	placate platitude	plead	plenitude	plumb	plus	podium	poignancy	polemic	polemical	policy	polity	pomp pontifical	postulate	practice	practitioner	prattle	precept	precision	predecessor	preen	premise preternatural	procedural	proceeding	proceeds	proclivity	prognosticate	project	projection	proletarian promiscuous	propensity	property	propitiate	propitious	propriety	prospective	provender	provoke prowess	proxy	prude	pucker	puissant	pummel	pundit	purport	purse	purvey	pusillanimous	quaff quagmire	quail	querulous	quip	rail	raiment	rakish	ramify	rampart	rancor	range	ranging	rankle rapt	rarefied	ratify	rationalism	rave	ravel	reactionary	readjustment	rebuke	rebus	recondite recourse	recovering	redolent	redoubtable	refectory	reflect	refractory	rejoinder	remission	rend render	renegade	repast	repeal	repine	reprise	reprobation	resonant	resource	restrained	retainer retentive	reticent	retinue	retiring	retort	retrench	revelry	reverie	revulsion	rhetoric	rider	rile ripple	rococo	roil	roseate	rout	rubric	rudiment	ruse	salient	sanction	sate	satire	saturnine savant	scabbard	scale	scarce	scheme	scintilla	scrupulous	scrutinize	sedulous	seemly	seethe semblance	sententious	sentient	sentinel	sextant	sherbet	siege	simper	sinecure	sinuous	skill skittish	slay	sleight	slew	sluice	smattering	sob	sonorous	spangle	spat	spate	specious	spectral squalor	squeamish	staid	stake	stanch	steep	stentorian	stilted	stint	stipulate	stock	straight stratum	striate	stricture	stultify	stump	stupendous	suavity	subaltern	subjugate	sublimate	sublime subside	substantial	succor	suffrage	sully	sultry	superannuated	supercilious	suppliant	surly surreptitious	suspended	swerve	sybarite	sycophant	syllogism	symmetry	tarry	tawdry	teem	temper temporize	tenable	tenacious	territory	testator	theory	thrall	threadbare	throng	tide	toil	tour	tout tract	traduce	trappings	tread	trenchant	tribunal	trite	triumvirate	troth	turbid	tureen	turgid tutelage	tyro	ulterior	unaffected	unanimity	unassuming	unconscionable	uncouth	unction	undertake undertaking	unexceptionable	unfaltering	ungainly	unimpeachable	unmitigated	unseemly	untoward unwonted	upbraid	upshot	urbane	usurp	utter	vacate	vagary	vain	vapid	vassal	vat	venerable venerate	venial	venture	veracious	veritable	vestige	vestment	vex	vilify	vintner	virtuoso	visage viscid	viscous	vitiate	vociferous	voluble	waffle	waive	wander	wanton	warble	warrant	wax	waylay weigh	welt	wheedle	whet	whittle	whorl	willful	wistful	wizened	wont	wraith	wrangle	wreak	wry yeoman	yield	zealot	zenith	zephyr];
+
+var gameStarted = false;
+var currentWord;
+var wordAsDashes;
+var guessesRemaing;
+var lettersGuessed;
+var correctGuesses;
+var numWins = 0;
+var numLosses = 0;
+var getNewWord;
+var wordPlace; 
+var wordCharArray = [];
+var dashesArray = [];
+
+function startGame() {
+	gameStarted = true;
+	lettersGuessed = [];
+	correctGuesses = 0;
+	wordPlace = Math.floor(Math.random() * wordBank.length);
+	currentWord = wordBank[wordPlace];
+	guessesRemaing = 15 - currentWord.length;		
+	wordAsDashes = makeIntoDashes(currentWord);	
+	wordCharArray = currentWord.split('');
+	dashesArray = wordAsDashes.split('');
+	document.getElementById("currentWord").innerHTML = wordAsDashes;
+	document.getElementById("lettersGuessed").innerHTML = "--";
+	document.getElementById("guessesRemaing").innerHTML = guessesRemaing;
+}
+
+// display number of _ equal to word length
+function makeIntoDashes(word) {
+	var dashes = "";
+	for (i = 0; i < word.length - 1; i++) {
+		dashes += "_ ";
+	}
+	dashes += "_";
+	return dashes;
+}
+
+// event listener for keystrokes
+function playGame(letter) {
+	var letter = letter.toLowerCase();
+
+	// check if key input is a letter
+	if (alphabet.indexOf(letter) > -1) {
+		if (wordCharArray.indexOf(letter) > -1) {
+			correctGuesses++;
+			showLetter(letter);
+		}
+		else {
+			if (lettersGuessed.indexOf(letter) > -1) {
+				return;
+			}
+			else {
+				guessesRemaing--;
+				document.getElementById("guessesRemaing").innerHTML = guessesRemaing;
+				lettersGuessed.push(letter);
+				document.getElementById("wrongGuesses").innerHTML = lettersGuessed.join(' ');
+				if (guessesRemaing == 0) {
+					alert("Sorry! The correct answer is " + currentWord);
+					startGame();
+					numLosses++;
+					document.getElementById("losses").innerHTML = numLosses;
+				}
+			}
+		}
+	}
+}
+
+// shows letter in correct location if it's in current word
+function showLetter(letter) {
+	
+	for (i = 0; i < currentWord.length; i++) {
+		if (letter == wordCharArray[i]) {
+			dashesArray[i * 2] = letter;
+			console.log(dashesArray);
+		}
+	}
+	document.getElementById("currentWord").innerHTML = dashesArray.join("");
+    checkWin();
+}
+
+document.onkeyup = function (event) {
+
+	if (!gameStarted) {
+
+		document.getElementById("letsPlay").innerHTML = "";
+		startGame();
+		document.getElementById("currentWord").innerHTML = wordAsDashes.split(",");
+		console.log(currentWord);
+        gameStarted = true;
+        
+	} else {
+
+		playGame(event.key);
+	}
+}
+
+function checkWin() {
+
+	if (dashesArray.indexOf("_") === -1) {
+
+		alert("WINNER WINNER CHICKEN DINNER! The answer is " + currentWord);
+		winCount++;
+		document.getElementById("wins").innerHTML = winCount;
+        startGame();
+        
+	}
+}
